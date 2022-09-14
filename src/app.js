7fe3f12691441ea86032dbbd15a467b3b0627b9a
Tales <jobbtales@gmail.com>
2022-09-14 18:46:51 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { startDatabase } from "./database/index.js";
+import usersRoutes from "./routes/users.routes.js"
+import teamsRoutes from "./routes/teams.routes.js"
+import cognitiveRoutes from "./routes/cognitive.routes.js"
+import cors from "cors"
+
+const app = express()
+const port = 3030
+
+app.use(express.json())
+
+app.use("/users", cors(), usersRoutes)
+app.use("/teams", cors(), teamsRoutes)
+app.use("/cognitiveloadmeter", cors(), cognitiveRoutes)
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+    startDatabase()
+})
